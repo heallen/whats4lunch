e16bf05e83e379187fe486169876291d2b0bbca6
Allen He <allenhe97@yahoo.com>
2018-04-05 17:20:01 -0400

    Created connection pools

--- app.js
+++ app.js
@@ -1,37 +1,56 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => res.send('Hello World!'))
-
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
 
 var oracledb = require('oracledb');
 
-function getConnection(callback) {
-    oracledb.getConnection(
-      {
+function init(){
+    oracledb.createPool(
+        {
         user          : "allen",
         password      : "password",
         connectString : "(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)\
         (HOST=whats4lunch.cvvc4g5pnndo.us-east-1.rds.amazonaws.com)(PORT=1521))(CONNECT_DATA=(SERVER=DEDICATED)\
         (SERVICE_NAME=orcl)))"
-      }, callback) // callback(err, connection)
+      }, function(err, pool){
+        if (err) {
+          console.error(err.message);
+          return;
+        }
+        app.listen(3000, () => console.log('Example app listening on port 3000!'))
+        app.get('/', (req, res) => mainPage(req, res, pool))
+      }
+    )
 }
 
-function releaseConnection(connection) {
-      connection.close(
-        function(err) {
-          if (err)
-            console.error(err.message);
-        });
+function mainPage(req, res, pool) {
+    getConnection(pool, createTables)
+    res.send('Hellow world!')
+}
+
+//call this function with the function callback(connection), and do whatever with that connection
+function getConnection(pool, callback) {
+    pool.getConnection(
+        function(err, connection) {
+            if (err) {
+              console.error(err.message);
+              return;
+            } 
+            //perform action with connection then close it
+            callback(connection, function(){
+                connection.close(
+                    function(err) {
+                      if (err)
+                        console.error(err.message);
+                    }
+                );
+            });
+          }
+    )
 }
 
 // follow this model to execute whatever sql queries
-function createTables(err, connection) {
-    if (err) {
-      console.error(err.message);
-      return;
-    }
+function createTables(connection) {
     connection.execute(
       `SELECT manager_id, department_id, department_name
        FROM departments
@@ -40,12 +59,10 @@ function createTables(err, connection) {
       function(err, result) {
         if (err) {
           console.error(err.message);
-          releaseConnection(connection);
           return;
         }
         console.log(result.rows);
-        releaseConnection(connection);
     });
 }
 
-getConnection(createTables)
+init()
